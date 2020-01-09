9cc8e9d96f67e88b179d3e53f477a570d095b156
Mauricio Pereira <mauriciojuniordesigner@gmail.com>
2020-01-09 08:49:46 -0300

    alterando view home

--- sistemavue/src/App.vue
+++ sistemavue/src/App.vue
@@ -1,32 +1,238 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app id="app">
+    
+    <v-navigation-drawer
+      v-model="drawer"
+      :clipped="$vuetify.breakpoint.lgAndUp"
+      app
+    >
+      <v-list dense>
+
+        <template>
+          <v-list-item>
+            <v-list-item-action>
+              <v-icon>home</v-icon>
+            </v-list-item-action>
+            <v-list-item-title>
+              Inicio
+            </v-list-item-title>
+          </v-list-item>
+        </template>
+
+        <template>
+          <v-list-group>
+
+            <v-list-item slot="activator">
+              <v-list-item-content>
+                Armazém
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item :to="{name: 'categoria'}">
+              <v-list-item-action>
+                <v-icon>table-chart</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title>
+                  Categorias
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item :to="{name: ''}">
+              <v-list-item-action>
+                <v-icon>table-chart</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title>
+                  Artigos
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+
+          </v-list-group>
+        </template>
+
+        <template>
+          <v-list-group>
+
+            <v-list-item slot="activator">
+              <v-list-item-content>
+                Compras
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item :to="{name: ''}">
+              <v-list-item-action>
+                <v-icon>table-chart</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title>
+                  Compras
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item :to="{name: ''}">
+              <v-list-item-action>
+                <v-icon>table-chart</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title>
+                  Fornecedores
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+
+          </v-list-group>
+        </template>
+
+        <template>
+          <v-list-group>
+
+            <v-list-item slot="activator">
+              <v-list-item-content>
+                Vendas
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item :to="{name: ''}">
+              <v-list-item-action>
+                <v-icon>table-chart</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title>
+                  Vendas
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item :to="{name: ''}">
+              <v-list-item-action>
+                <v-icon>table-chart</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title>
+                  Clientes
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+
+          </v-list-group>
+        </template>
+
+        <template>
+          <v-list-group>
+
+            <v-list-item slot="activator">
+              <v-list-item-content>
+                Acessos
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item :to="{name: ''}">
+              <v-list-item-action>
+                <v-icon>table-chart</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title>
+                  Usuários
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+
+          </v-list-group>
+        </template>
+
+        <template>
+          <v-list-group>
+
+            <v-list-item slot="activator">
+              <v-list-item-content>
+                Consultas
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item :to="{name: ''}">
+              <v-list-item-action>
+                <v-icon>table-chart</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title>
+                  Consulta Compras
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item :to="{name: ''}">
+              <v-list-item-action>
+                <v-icon>table-chart</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title>
+                  Consulta Vendas
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+
+          </v-list-group>
+        </template>
+
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      :clipped-left="$vuetify.breakpoint.lgAndUp"
+      app
+      color="blue darken-3"
+      dark
+    >
+      <v-toolbar-title
+        style="width: 300px"
+        class="ml-0 pl-3"
+      >
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+        <span class="hidden-sm-and-down">Sistema</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>logout</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <v-container
+        fluid
+        fill-height
+      >
+        <v-slide-y-transition mode="out-in">
+          <router-view/>
+        </v-slide-y-transition>
+      </v-container>
+    </v-content>
+
+    <v-footer height="auto">
+      <v-layout justify-center>
+        <v-flex text-xs-center>
+          <v-card flat tile color="primary" class="white--text">
+            <v-card-text class="white--text pt-0">
+              Mauricio PC Junior &copy; 2019
+            </v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-footer>
+
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import HelloWorld from './components/HelloWorld';
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default {
+  name: 'App',
+  data: () => ({
+    drawer: null
+  })
+};
+</script>
